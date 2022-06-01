63439d7cd60fe0c3dc5958d9da312e0b7fa08725
Yann <ybarlet@gmail.com>
2022-06-01 10:53:55 +0200

    Add photographer's infos and style

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,5 +1,5 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, tagline, price } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
@@ -9,8 +9,17 @@ function photographerFactory(data) {
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+        const div = document.createElement( 'div');
+        div.textContent = `${city}, ${country}`;
+        const p = document.createElement('p');
+        p.textContent = tagline;
+        const span = document.createElement('span');
+        span.textContent = `${price}€/jour`;
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(div);
+        article.appendChild(p);
+        article.appendChild(span);
         return (article);
     }
     return { name, picture, getUserCardDOM }
